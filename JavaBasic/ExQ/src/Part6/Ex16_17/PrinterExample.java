7413c2ffba0800a9a6ede63ec4a28f81f591d4d0
kwackbackE <ehdgur0591@gmail.com>
2022-01-11 21:17:26 +0900

    Update ExQ > Part6 > Ex16-17

---
+++ JavaBasic/ExQ/src/Part6/Ex16_17/PrinterExample.java
@@ -0,0 +1,11 @@
+package Part6.Ex16_17;
+
+public class PrinterExample {
+    public static void main(String[] args) {
+        Printer p1 = new Printer();
+        p1.println(10);
+        p1.println(true);
+        p1.println(5.77);
+        p1.println("유동혁");
+    }
+}
