0c74d4baaaa6b546383adcde562b9719e06b7670
KwackbackE <ehdgur0591@gmail.com>
2022-01-11 11:01:33 +0900

    Finsh Part 6_11_2_staticFinal

---
+++ JavaBasic/classing(Part6~)/Part6_class2/src/Part6_11_2_staticFinal/Earth_Driver.java
@@ -0,0 +1,8 @@
+package Part6_11_2_staticFinal;
+
+public class Earth_Driver {
+    public static void main(String[] args) {
+        System.out.println("지구반지름: " + Earth.EARTH_RADIUS);
+        System.out.println("지구표면적: " + Earth.EARTH_SURFACE_AREA);
+    }
+}
