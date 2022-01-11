6521d696bf12a7f6868549100c0fa805ce311dd2
kwackbackE <ehdgur0591@gmail.com>
2022-01-11 22:37:28 +0900

    Update ExQ > Part6 > Ex19_20 : Ex19(ing...)

--- JavaBasic/ExQ/src/Part6/Ex19_20/Account.java
+++ JavaBasic/ExQ/src/Part6/Ex19_20/Account.java
@@ -1,20 +1,10 @@
 package Part6.Ex19_20;
 
 public class Account {
-    static final int MIN_BALANCE =0;
-    static final int MAX_BALANCE =1000000;
+    private String account_number;
+    private String owner;
+    private int balance;
 
-    private int balance=0;
+    //public Account(String account_number)
 
-    public int getBalance() {
-        return balance;
-    }
-
-    public void setBalance(int balance){
-        if (balance<MIN_BALANCE || balance>MAX_BALANCE){
-            return;
-        } else {
-            this.balance = balance;
-        }
-    }
 }
