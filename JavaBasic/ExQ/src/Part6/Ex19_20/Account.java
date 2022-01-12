85df61c8ddf9a23d6cc9a6ecb7b7d8bc9f487259
KwackbackE <ehdgur0591@gmail.com>
2022-01-12 13:31:29 +0900

    Exq > Part6 > Ex19_20 (ing...)

--- JavaBasic/ExQ/src/Part6/Ex19_20/Account.java
+++ JavaBasic/ExQ/src/Part6/Ex19_20/Account.java
@@ -5,6 +5,18 @@ public class Account {
     private String owner;
     private int balance;
 
-    //public Account(String account_number)
+    public Account(String account_number, String owner, int balance) {
+        this.account_number = account_number;
+        this.owner = owner;
+        this.balance = balance;
+    }
 
+    public Account() {}
+
+    public String getAccount_number() {return account_number;}
+    public void setAccount_number(String account_number) {this.account_number = account_number;}
+    public String getOwner() {return owner;}
+    public void setOwner(String owner){this.owner = owner;}
+    public int getBalance() {return balance;}
+    public void setBalance(int balance) {this.balance = balance;}
 }
--- JavaBasic/ExQ/src/Part6/Ex19_20/AccountExample.java
+++ JavaBasic/ExQ/src/Part6/Ex19_20/AccountExample.java
@@ -1,20 +1,126 @@
 package Part6.Ex19_20;
 
+import java.util.Scanner;
+
 public class AccountExample {
+    private static Scanner sc = new Scanner(System.in);
+    private static Account[] accountArray = new Account[100];
+    private static int check = 0;
+
     public static void main(String[] args) {
-        Account a1 = new Account();
+        boolean run= true;
 
-        a1.setBalance(10000);
-        System.out.println("현재 잔고: " + a1.getBalance());
+        while (run){
+            System.out.println("-------------------------------------------------");
+            System.out.println("1.계좌생성 | 2.계좌목록 | 3.예금 | 4.출금 | 5.출금");
+            System.out.println("-------------------------------------------------");
+            System.out.print("선택> ");
+            int N = sc.nextInt();
 
-        a1.setBalance(-100);
-        System.out.println("현재 잔고: " + a1.getBalance());
+            switch (N) {
+                case 1:
+                    createAccount();
+                    break;
+                case 2:
+                    accountList();
+                    break;
+                case 3:
+                    deposit();
+                    break;
+                case 4:
+                    withdraw();
+                    break;
+                case 5:
+                    run = false;
+                    System.out.println("프로그램종료");
+                    break;
+                default:
+                    System.out.println("잘못입력");
+                    break;
+            }
+        }
+    }
+    private static void createAccount() {
+        String account_number;
+        String name;
+        int balance;
 
-        a1.setBalance(2000000);
-        System.out.println("현재 잔고: " + a1.getBalance());
+        System.out.println("----------");
+        System.out.println("계좌생성");
+        System.out.println("----------");
+        System.out.print("계좌번호: ");
+        account_number = sc.next();
+        System.out.print("계좌주: ");
+        name = sc.next();
+        System.out.print("초기입금액: ");
+        balance = sc.nextInt();
+        accountArray[check] = new Account(account_number, name, balance);
+        check++;
+    }
 
-        a1.setBalance(300000);
-        System.out.println("현재 잔고: " + a1.getBalance());
+    private static void accountList() {
+        System.out.println("-----------------");
+        System.out.println("계좌목록");
+        System.out.println("-----------------");
 
+        for (int i=0 ; i<check; i++){
+            System.out.print(accountArray[i].getAccount_number()+" ");
+            System.out.print(accountArray[i].getOwner()+" ");
+            System.out.print(accountArray[i].getBalance());
+            System.out.println();
+        }
+
+    }
+
+    private static void deposit() {
+        Account tmp = new Account();
+
+        System.out.println("-----------------");
+        System.out.println("예금");
+        System.out.println("-----------------");
+        System.out.print("계좌번호");
+        String an = sc.next();
+        System.out.println();
+
+        tmp = findAccount(an); // 계좌번호 넘김
+        System.out.print("예금액: ");
+        int money = sc.nextInt();
+        int balence = tmp.getBalance();
+        tmp.setBalance(balence+money);
+        System.out.println();
+        System.out.println("예금 성공");
+
+    }
+    private static void withdraw() {
+        Account tmp = new Account();
+
+        System.out.println("-----------------");
+        System.out.println("출금");
+        System.out.println("-----------------");
+        System.out.print("계좌번호");
+        String an = sc.next();
+        System.out.println();
+        tmp = findAccount(an);
+        System.out.print("출금액: ");
+        int money = sc.nextInt();
+        int blanec =tmp.getBalance();
+        tmp.setBalance(blanec-money);
+        System.out.println();
+        System.out.println("출금 성공");
+
+
+    }
+
+    private static Account findAccount(String account_number){
+        Account tmp = new Account();
+        for (int i=0; i<check; i++){
+            String account = accountArray[i].getAccount_number();
+            if (account_number.equals(account)){
+                tmp = accountArray[i];
+            } else {
+                System.out.println("계좌불일치");
+            }
+        }
+        return tmp;
     }
 }
