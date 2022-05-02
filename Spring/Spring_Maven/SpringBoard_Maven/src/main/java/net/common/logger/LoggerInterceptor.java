a33e5b60d67df7835e52b6662f56a5b830e62ead
kwackbackE <ehdgur0591@gmail.com>
2022-05-02 21:38:14 +0900

    프로젝트-리스트 보기

---
+++ Spring/Spring_Maven/SpringBoard_Maven/src/main/java/net/common/logger/LoggerInterceptor.java
@@ -0,0 +1,8 @@
+package net.common.logger;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LoggerInterceptor extends HandlerInterceptorAdapter{
+	prote
+
+}
---
+++ Spring/Spring_Maven/SpringBoard_Maven/src/main/java/net/sample/service/SampleServiceImpl.java
@@ -0,0 +1,46 @@
+package net.sample.service;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+import net.sample.dao.SampleDAO;
+
+@Service("sampleService")
+public class SampleServiceImpl implements SampleService{
+	@Resource(name="sampleDAO")
+	private SampleDAO sampleDAO;
+	
+	@Override
+	public List<Map<String, Object>> selectBoardList(Map<String, Object> map) throws Exception {
+		return sampleDAO.selectBoardList(map);
+	}
+
+	@Override
+	public void insertBoard(Map<String, Object> map) throws Exception {
+		sampleDAO.insertBoard(map);
+		
+	}
+
+	@Override
+	public Map<String, Object> selectBoardDetail(Map<String, Object> map) throws Exception {
+		sampleDAO.updateHitCnt(map);
+		Map<String, Object> resultMap = sampleDAO.selectBoardDetail(map);
+		return resultMap;
+	}
+
+	@Override
+	public void updateBoard(Map<String, Object> map) throws Exception{
+		sampleDAO.updateBoard(map);
+	}
+
+	@Override
+	public void deleteBoard(Map<String, Object> map) throws Exception {
+		sampleDAO.deleteBoard(map);
+	}
+
+}
